66b23920f4d6c55707af9a1883a385e2ae4cc10e
Muhammad Raffi <raffikecil85@gmail.com>
2022-06-02 22:01:03 +0700

    Upload

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const {DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 
 module.exports = {
@@ -6,35 +8,23 @@ module.exports = {
         password: DB_PASSWORD,
         database: `${DB_NAME}`,
         host: DB_HOST,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
+        dialect: "postgres",
+        ssl: true,
     },
     test: {
         username: DB_USERNAME,
         password: DB_PASSWORD,
         database: `${DB_NAME}_test`,
         host: DB_HOST,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
+        dialect: "postgres",
+        ssl: true,
     },
     production: {
         username: DB_USERNAME,
         password: DB_PASSWORD,
         database: `${DB_NAME}_production`,
         host: DB_HOST,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
+        dialect: "postgres",
+        ssl: true,
     },
 };
