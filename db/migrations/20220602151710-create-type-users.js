1fc451203ed95c3ce2cb35e2ed167051d110c600
Muhammad Raffi <raffikecil85@gmail.com>
2022-06-02 22:18:59 +0700

    Update

--- db/migrations/20220602151710-create-type-users.js
+++ db/migrations/20220602151710-create-type-users.js
@@ -1,30 +1,27 @@
-"use strict";
+'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Posts", {
+    await queryInterface.createTable('Type_Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER
       },
-      title: {
-        type: Sequelize.STRING,
-      },
-      body: {
-        type: Sequelize.TEXT,
+      type_user: {
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE,
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-      },
+        type: Sequelize.DATE
+      }
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Posts");
-  },
+    await queryInterface.dropTable('Type_Users');
+  }
 };
