bad1372544cf265d6e278af3e9b46f5d23a4eb2d
Muhammad Raffi <raffikecil85@gmail.com>
2022-06-03 10:20:19 +0700

    Update

--- db/seeds/20220603020039-cars.js
+++ db/seeds/20220603020039-cars.js
@@ -639,6 +639,6 @@ module.exports = {
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.bulkDelete("People", null, {});
+        await queryInterface.bulkDelete("Cars", null, {});
     },
 };
--- db/seeds/20220603020106-type_users.js
+++ db/seeds/20220603020106-type_users.js
@@ -26,6 +26,6 @@ module.exports = {
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.bulkDelete("People", null, {});
+        await queryInterface.bulkDelete("Type_Users", null, {});
     },
 };
